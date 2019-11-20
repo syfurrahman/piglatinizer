7fa7ccd1c63f512f0fe237aa351044df6d1e2de9
syfurrahman <srahman5313.student@hsfi.us>
2019-11-20 22:50:58 +0000

    Pig Latin completed Project

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
-$( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("document").read(function(){
+$("#pigButton").click(function() {
+   let inputtoChange= $("#word").val();
+    inputtoChange = inputtoChange.toLowerCase();
 
+   const vowels = ["a", "e", "i", "o", "u"];
 
+   let wordtoChange = inputtoChange.split(" ");
 
+   let result = [];
+   
+   wordtoChange.forEach(function(word) {
+    
+    let vowelIndex = 0;
+
+    if (vowels.includes(word[0])) {
+    
+   result.push( word + "ay"+ " ");
+    
+    } else {
+   
+    for (let char of word) {
+      
+      if (vowels.includes(char)) {
+       
+        vowelIndex = word.indexOf(char);
+        break;
+      }
+    }
+
+    result.push(word.slice(vowelIndex) + word.slice(0, vowelIndex) + "ay"+" ");
+    
+    }
+   
+    
+    $("#pigWord").html(result);
+    
+    });
   
-  
-  
-  
+  });
+
 });
